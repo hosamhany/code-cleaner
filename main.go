4ec57bf288fde630c78a9fbff297fe007fbd64a9
Hosam Hany <hossam.eldin.hany@gmail.com>
2025-03-11 22:38:25 +0100

    Add lines to be removed

--- main.go
+++ main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	// > Start clean up
+	fmt.Println("This part should be cleaned up")
+	// > End clean up
 	fmt.Println("This part should not be cleaned up")
 }
