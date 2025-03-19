c30b132c369a642c5dfabbd29e1099f96e2cd26d
Hosam Hany <hossam.eldin.hany@gmail.com>
2025-03-19 12:59:00 +0100

    Adjust main.go to support end date

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import "fmt"
 func main() {
 	// > Start clean up at 2042-01-01
 	fmt.Println("This part should not be cleaned up yet")
+	// > Start clean up at 2023-01-01
+	fmt.Println("This part should also be cleaned up")
+	// > End clean up at 2023-01-01
 	// > End clean up at 2042-01-01
 	fmt.Println("This part should not be cleaned up")
 }
